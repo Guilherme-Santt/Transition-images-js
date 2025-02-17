674ac8ca5b4049730bd09064a21c0978e45f13a3
Gui Sant <guisant@icloud.com>
2025-02-17 13:58:22 -0300

    Separando para uma função e otimizando código

--- main.js
+++ main.js
@@ -7,12 +7,9 @@ const img = document.querySelector('#img')
 const input = document.querySelector('#animal');
 
 
-
-input.addEventListener('keydown', (event) =>{
-    console.log(event)
-
+function transition(event){
     if(event.key !== "Enter") return
-    
+
     const valueinput = input.value;
     act.innerHTML = `Ativamos o ${valueinput}`
     act.classList.add('active')
@@ -22,19 +19,8 @@ input.addEventListener('keydown', (event) =>{
         img.src = "" + valueinput + ".jpeg"; // Muda a imagem
         img.classList.remove('opacity'); // Remove a classe de opacidade para mostrar a nova imagem
     }, 200); // O tempo deve ser igual ao tempo da transição CSS
-    
-});
+}
 
 
-btn.addEventListener('click', () =>{
-    console.log("clicou no botão");
-    const valueinput = input.value;
-    act.innerHTML = `Ativamos o ${valueinput}`
-    act.classList.add('active')
-    img.classList.toggle('opacity')
-    
-    setTimeout(() => {
-        img.src = "" + valueinput + ".jpeg"; // Muda a imagem
-        img.classList.remove('opacity'); // Remove a classe de opacidade para mostrar a nova imagem
-    }, 200); // O tempo deve ser igual ao tempo da transição CSS
-});
+btn.addEventListener('click', transition);
+window.addEventListener('keydown', transition);
